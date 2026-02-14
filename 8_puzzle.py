e5b9011f5beb493cbd47877632c3c9a3cc43a75b
MaanasKollegal <mkoll007@ucr.edu>
2026-02-13 21:36:58 -0800

    Goal Position Function

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -1,4 +1,5 @@
 import heapq
+import copy
 
 #Goal State table which represents the solved puzzle
 Goal_State = [
@@ -6,6 +7,17 @@ Goal_State = [
 [4, 5, 6],
 [7, 8, 0]
 ]
+
+#Computes goal positions for heuristic calculation 
+Goal_Position = {}
+for r, row in enumerate(Goal_State):
+    for c, val in enumerate(Goal_State):
+        Goal_Position[val] = (r, c)
+
+N_Rows = len(Goal_State)
+C_Cols = len(Goal_State[0])
+
+
 
 def CreateNode(state, parent=None, g=0, h=0, depth=0): #Creates a search node dictionary, f = g + h
     return{
@@ -15,5 +27,10 @@ def CreateNode(state, parent=None, g=0, h=0, depth=0): #Creates a search node di
         "h": h, #cost estimate 
         "f": g + h, #total cost
         "depth": depth
+    }
+
+def CreateQueue(node): #Creates the queue which starts out with one node
+    {
+
     }
     
