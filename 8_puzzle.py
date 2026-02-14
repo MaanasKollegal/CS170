2d0352fe8b3eb087520ad4f4187a67bc18822f2a
MaanasKollegal <mkoll007@ucr.edu>
2026-02-13 22:09:37 -0800

    Heuristics

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -32,6 +32,35 @@ def find_blank(state):
                 return r, c
     raise ValueError("No blank tile found.")
 
+def MakeMove(state, r, c, nr, nc):
+    if nr < 0 or nr >= N_Rows or nc < 0 or nc >= N_Cols:
+        return None
+    new_state = copy.deepcopy(state)
+    new_state[r][c], new_state[nr][nc] = new_state[nr][nc], new_state[r][c]
+    return new_state
+
+#Move tile up
+def move_up(state):
+    r, c = find_blank(state)
+    return MakeMove(state, r, c, r - 1, c)
+
+#Move tile down
+def move_down(state):
+    r, c = find_blank(state)
+    return MakeMove(state, r, c, r + 1, c)
+
+#Move tile left
+def move_left(state):
+    r, c = find_blank(state)
+    return MakeMove(state, r, c, r, c - 1)
+
+#Move tile right
+def move_right(state):
+    r, c = find_blank(state)
+    return MakeMove(state, r, c, r, c + 1)
+
+
+OPERATORS = [move_up, move_down, move_left, move_right]    
 
 
 
@@ -47,6 +76,32 @@ def CreateNode(state, parent=None, g=0, h=0, depth=0): #Creates a search node di
 
 
 def CreateQueue(node): #Creates the queue which starts out with one node
-    {
+    counter = [0]
+    return[(node["f"], 0, node)], counter
 
-    }
+
+#Counts tiles which are misplaced (tiles that are not in the goal position)
+def H_MisplacedTile(state):
+    count = 0
+    for r in range(N_Rows):
+        for c in range(N_Cols):
+            val = state[r][c]
+            if val != 0 and state[r][c] != Goal_State[r][c]:
+                count += 1 
+    return count
+
+
+#Finds Manhattan Distance
+#Sum of |current_row - goal_row| + |current_col - goal_col| for each non-blank tile.
+def H_Manhattan(state):
+    total = 0
+    for r in range(N_Rows):
+        for c in range(N_Cols):
+            val = state[r][c]
+            if val != 0:
+                gr, gc = Goal_Position[val]
+                total += abs(r - gr) + abs(c - gc)
+    return total
+
+    
+        
