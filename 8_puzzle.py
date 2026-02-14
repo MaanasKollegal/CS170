869387a9ebf0d8822569788d1562c33eb7634ae2
MaanasKollegal <mkoll007@ucr.edu>
2026-02-13 21:04:46 -0800

    New function Node

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -1,7 +1,19 @@
 import heapq
 
+#Goal State table which represents the solved puzzle
 Goal_State = [
 [1, 2, 3],
-[4, 5, ],
-[7, 8, 9]
+[4, 5, 6],
+[7, 8, 0]
 ]
+
+def CreateNode(state, parent=None, g=0, h=0, depth=0): #Creates a search node dictionary, f = g + h
+    return{
+        "state": state,
+        "parent": parent,
+        "g": g, #cost
+        "h": h, #cost estimate 
+        "f": g + h, #total cost
+        "depth": depth
+    }
+    
