5f64c3ff1b63e718b13e9cac7163780a3762d583
MaanasKollegal <mkoll007@ucr.edu>
2026-03-15 19:47:02 -0700

    Forward Selection

--- feature_selection.py
+++ feature_selection.py
@@ -1,4 +1,3 @@
-#temporary
 
 import math
 import sys
@@ -16,6 +15,11 @@ def load_data(filename):
     return data
 
 
+#Format a set of feature numbers as {a,b,c} sorted.
+def set_str(s):
+    return "{" + ",".join(str(x) for x in sorted(s)) + "}"
+ 
+
 def cross_validation(data, current_set, feature_to_add, best_so_far=0.0):
     features = set(current_set)
     if feature_to_add is not None:
@@ -31,7 +35,7 @@ def cross_validation(data, current_set, feature_to_add, best_so_far=0.0):
         nearest_label = None
 
         for k in range(n):
-            if k == 1:
+            if k == i:
                 continue # skip self
             
             dist = 0.0
@@ -57,4 +61,46 @@ def cross_validation(data, current_set, feature_to_add, best_so_far=0.0):
 
 
 def forward_selection(data): 
-    num_features 
+    num_features = len(data[0]) - 1
+
+    all_features = set(range(1, num_features + 1))
+    full_acc = cross_validation(data, all_features, None)
+    print(f"\nRunning nearest neighbor with all {num_features} features, "
+          f"using \"leaving-one-out\" evaluation, I get an accuracy of {full_acc * 100:.1f}%")
+    
+    print("\nBeginning search.\n")
+
+    current_set = set()
+    best_overall_accuracy = 0.0
+    best_overall_features = set()
+
+    for level in range(1, num_features + 1):
+        best_accuracy_this_level = 0.0
+        feature_to_add_this_level = None
+ 
+        for f in range(1, num_features + 1):
+            if f in current_set:
+                continue
+ 
+            accuracy = cross_validation(data, current_set, f, best_accuracy_this_level)
+            candidate = current_set | {f}
+            print(f"\t\tUsing feature(s) {set_str(candidate)} accuracy is {accuracy * 100:.1f}%")
+
+            if accuracy > best_accuracy_this_level:
+                best_accuracy_this_level = accuracy
+                feature_to_add_this_level = r
+
+        current_set = current_set | {feature_to_add_this_level}
+
+        if best_accuracy_this_level < best_overall_accuracy:
+            print(f"\n(Accuracy has decreased, continuing search in case of local maxima)")
+
+        else: 
+            best_overall_accuracy = best_accuracy_this_level
+            best_overall_features = set(current_set)
+
+            print(f"Feature set {set_str(current_set)} was best, accuracy is "
+              f"{best_accuracy_this_level * 100:.1f}%\n")
+            
+    print(f"Finished search. Best feature subset is {set_str(best_overall_features)}, "
+          f"which has an accuracy of {best_overall_accuracy * 100:.1f}%")
